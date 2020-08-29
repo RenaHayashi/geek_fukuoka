e7a42d0277c0133ebf3d5a8b354cc7480bfd9a29
github RenaHayashi <rena.hayashi.route66@gmail.com>
2020-08-29 16:55:09 +0900

    dataの表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button v-on:click="postTweet">
+      クリック
+    </button>
+
+    <div v-for="(tweet, index) in tweets" v-bind:key="index">
+      {{ tweet.text }}
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import db from "@/firebase";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  name: "home",
+  methods: {
+    postTweet: function() {
+      db.collection("tweets").add({
+        text: "こんにちは、ツイートの本文です。"
+      });
+    }
+  },
+  data: function() {
+    return {
+      text: "こんにちは",
+      tweets: [
+        { id: 1, text: "こんにちは" },
+        { id: 2, text: "おはよう" }
+      ]
+    };
   }
 };
 </script>
