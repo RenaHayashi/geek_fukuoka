44933030a7b1b4d467769560af24c531624e463c
github RenaHayashi <rena.hayashi.route66@gmail.com>
2020-08-30 11:52:24 +0900

    ページ作成

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>{{ text }}</span>
+    <input type="text" v-model="text" />
+    <button v-on:click="addText">追加</button>
+    <div v-for="(text, index) in texts" v-bind:key="index">
+      {{ text }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      texts: [],
+      text: ""
+    };
+  },
+  methods: {
+    addText: function() {
+      this.texts.push(this.text);
+      this.text = "";
+    }
+  }
+};
+</script>
